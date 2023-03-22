d78f0555db6edd9a2d8ca988a910b1eafb402398
lunaridelmar <lunaridelmar@gmail.com>
2023-03-21 20:35:34 -0500

    #Init: cleanup + properties

--- register-home-service/src/main/java/mar/del/lunari/registerhomeservice/RegisterHomeServiceApplication.java
+++ register-home-service/src/main/java/mar/del/lunari/registerhomeservice/RegisterHomeServiceApplication.java
@@ -2,10 +2,8 @@ package mar.del.lunari.registerhomeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "mar.del.lunari")
 public class RegisterHomeServiceApplication {
 
 	public static void main(String[] args) {
